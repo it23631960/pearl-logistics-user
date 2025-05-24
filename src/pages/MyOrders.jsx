a6c64b170570d82ca696fb2c63b58ea296caf88e
it23631960 <161797356+it23631960@users.noreply.github.com>
2025-05-24 19:36:14 +0530

    Update MyOrders.jsx

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -16,7 +16,7 @@ const MyOrders = () => {
   useEffect(() => {
     const fetchOrders = async () => {
       try {
-        const response = await axios.get(`http://localhost:8080/api/orders/user/${userId}`, {
+        const response = await axios.get(`${backendUrl}api/orders/user/${userId}`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
